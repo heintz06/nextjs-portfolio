f20b91300af9d58205d202af6d06b8127bfdc6e9
Marventz B <500173175@fvtc.edu>
2023-11-19 20:35:07 -0600

    checkpoint

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 const NavBar: React.FC = () => {
     return (        
-        <nav className="py-10 mb-12 flex justify-between">
+        <nav className="py-10 flex justify-between">
             <h1 className='text-xl font-burtons'>MB</h1>          
             <ul className='flex items-center gap-2'>
                 <li><Link href="/about">About</Link></li>
